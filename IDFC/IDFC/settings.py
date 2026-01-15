21983fdce7088b667ef76ad0e764a533d168a384
Kiran <kiranbodakhe@gmail.com>
2026-01-15 23:06:16 +0530

    Created PersonlLoan APP and Chekcked with Template files and Static files

--- IDFC/IDFC/settings.py
+++ IDFC/IDFC/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR=Path.joinpath(BASE_DIR, 'templates')
+STATIC_DIR=Path.joinpath(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'personlLoan'
 ]
 
 MIDDLEWARE = [
@@ -115,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
---
+++ IDFC/personlLoan/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+from  . import views
+
+urlpatterns = [
+    path('show/', views.show, name='show'),
+    path('home/', views.home, name='home'),
+
+
+]
